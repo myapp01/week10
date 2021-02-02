a9cfaed6d59c142d45ff4a97bfd2afe107733f5d
myapp01 <69285962+myapp01@users.noreply.github.com>
2021-02-02 11:26:02 +0700

    Add files via upload

---
+++ funtion302.js
@@ -0,0 +1,10 @@
+function myParaemter(a,b,x)
+{
+    //To do
+    //let a = 5;
+    //let b = 10;
+    let c = a+b+x ;
+    console.log(`${a} +  ${b}  =  ${c}`);
+
+}
+myParaemter(5,10,15); //calling
